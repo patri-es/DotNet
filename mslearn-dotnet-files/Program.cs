3d269a771fd5e665961748755a6bcf2f37fbb83a
Patricia <78591737+patri-es@users.noreply.github.com>
2025-10-17 12:50:03 +0200

    mslearn-dotnet-files add complete NO submodule

---
+++ mslearn-dotnet-files/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using System.Collections.Generic;
+
+// This is a simple example with a simple route in the folder structure... 
+var salesEasyFiles = FindEasyFiles("stores");
+foreach (var file in salesEasyFiles)
+{
+    Console.WriteLine(file);
+}
+// function with simple route that search and return de files in a folder.
+IEnumerable<string> FindEasyFiles(string folderName)
+{
+    List<string> salesFiles = new List<string>();
+
+    var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
+
+    foreach (var file in foundFiles)
+    {
+        // The file name will contain the full path, so only check the end of it
+        if (file.EndsWith("sales.json"))
+        {
+            salesFiles.Add(file);
+        }
+    }
+    return salesFiles;
+}
+
